edfcb46b27254d5753cdc49b689763c7735369a3
MattiaEsposito98 <esposito.matty@hotmail.it>
2024-12-06 14:51:36 +0100

    add prime due pagine header e info

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import { useState } from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import Info from './pages/Info'
 
 
 function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={HomePage} />
+        <Route path="/info" Component={Info} />
+      </Routes>
+    </BrowserRouter>
+
+  )
 
 
-  return <BrowserRouter></BrowserRouter>
+
 }
 
 export default App
---
+++ src/pages/Info.jsx
@@ -0,0 +1,3 @@
+export default function Info() {
+  return <h1>Informazioni</h1>
+}
